1642af3232806c84c03a12a2204011d2124a7d50
logan <loganelliott@protonmail.com>
2020-10-12 03:27:59 +0100

    bump version

--- shared/data.js
+++ shared/data.js
@@ -2,7 +2,7 @@
 /// <reference path="../node_modules/@citizenfx/client/index.d.ts" />
 
 CONFIG_C = {
-  version: '1.0.0',
+  version: '1.0.1',
   client_id: 'c92707c27c9e40f88b19315bb753d917',
   scope: 'user-read-currently-playing',
   redirect_uri: 'https://spotifive.services.dislike.life/auth/begin'
